9985e5cbc1882b8d89651cdcdad8ad08bd90aac5
Mark Cohen <Mark.Cohen1@marist.edu>
2023-03-02 21:34:20 -0500

    Adding handling for the local DB

--- hwstats/monitor.py
+++ hwstats/monitor.py
@@ -1,5 +1,5 @@
 import os
-from time import sleep
+from time import sleep, time
 
 import psutil
 from sqlalchemy import create_engine
@@ -9,20 +9,26 @@ from sqlalchemy.orm import Session
 from hwstats import models
 
 COLLECTION_INTERVAL = 0.1
+TIMEOUT = 30
 
 
 def main():
     """Main function"""
-    out_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "out")
+
+    base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+    out_dir = os.path.join(base_dir, "out")
     os.makedirs(out_dir, exist_ok=True)
     db_path = os.path.join(out_dir, "tmp_metrics.db")
     engine = create_engine(f"sqlite:///{db_path}", echo=True)
 
     models.Base.metadata.create_all(engine)
 
+    start_time = time()
     while True:
         collect_metrics(engine)
         sleep(COLLECTION_INTERVAL)
+        if time() - start_time > TIMEOUT:
+            break
 
 
 def collect_metrics(engine: Engine) -> None:
