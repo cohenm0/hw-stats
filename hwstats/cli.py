5b5aec46390369915d3e0c40cae7fe80bc03d06b
Mark Cohen <Mark.Cohen1@marist.edu>
2023-03-04 15:43:53 -0500

    Command line entry point

---
+++ hwstats/cli.py
@@ -0,0 +1,15 @@
+import asyncio
+
+from hwstats.frontend import start_app
+from hwstats.monitor import start_metrics_collection
+
+
+async def main():
+    """Start the app and start collecting metrics"""
+    start_metrics_collection()
+    start_app()
+    await asyncio.gather(start_app(), start_metrics_collection())
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
