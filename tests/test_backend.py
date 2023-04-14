03a937a52fd2403f3c2201e1d15b09c0f0abcd8d
Mark Cohen <Mark.Cohen1@marist.edu>
2023-04-13 23:07:26 -0400

    Added a system load test

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -7,12 +7,9 @@ from hwstats.models import Base
 
 
 def test_get_db_connection(db_path: str) -> None:
-    """Test the get_db_connection function"""
+    """Test that we can connect to the database and write db file to disk"""
     engine = get_db_connection(db_path)
     # The db is not created until we call create_all
     Base.metadata.create_all(engine)
     assert isinstance(engine, Engine)
     assert os.path.exists(db_path)
-
-    # Cleanup
-    os.remove(db_path)
