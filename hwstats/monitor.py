cbe80739e278e7b344bc6298c8cfa03083b6ce9b
Mark Cohen <Mark.Cohen1@marist.edu>
2023-04-25 02:08:00 -0400

    Fixes for concurrency issues
    We had been hitting an issue where query performance was very poor and
    the insertions kept hitting a database lock. By separating the psutil
    metrics collection into a separate thread and adding it's results to a
    Queue, we can write to the database from the queue at regular intervals
    the allows for better scheduling by the ORM.

--- hwstats/monitor.py
+++ hwstats/monitor.py
@@ -1,4 +1,7 @@
 import logging
+from queue import Queue
+from sqlite3 import OperationalError
+from threading import Thread
 from time import sleep, time
 
 import psutil
@@ -20,33 +23,58 @@ def start_metrics_collection(
     :param timeout: How long to collect metrics for. If 0, collect forever
     :param db_path: Path to the database file
     """
-    # SQLAlchemy logging: https://docs.sqlalchemy.org/en/20/core/engines.html#configuring-logging
-    logging.getLogger("sqlalchemy").setLevel(logging.WARNING)
-
     engine = get_db_connection(db_path)
     models.Base.metadata.create_all(engine)
+
+    data_queue = Queue()
+    db_writer_thread = Thread(
+        target=write_to_db, args=(engine, data_queue), kwargs={"interval": collection_interval}
+    )
+    db_writer_thread.start()
 
     start_time = time()
     logger.info("Starting collection")
     while True:
-        collect_metrics(engine)
+        for _process in psutil.process_iter():
+            try:
+                with _process.oneshot():
+                    _sysprocess = models.build_sysprocess_from_process(_process)
+                    _cpu = models.build_cpu_from_process(_process)
+                    _memory = models.build_memory_from_process(_process)
+                    _disk = models.build_disk_from_process(_process)
+
+                    data_queue.put((_sysprocess, _cpu, _memory, _disk))
+
+            # If the process dies before the iterator gets to it then skip it
+            except (psutil.NoSuchProcess, FileNotFoundError):
+                pass
+
         sleep(collection_interval)
         if time() - start_time > timeout and timeout != 0:
             logger.warning("Stopping collection")
+            data_queue.put(None)
+            db_writer_thread.join()
             return
 
 
-def collect_metrics(engine: Engine) -> None:
-    """Collect metrics from the system and store them in the database"""
+def write_to_db(engine: Engine, data_queue: Queue, interval: float = 0.1) -> None:
+    """Write data from the queue to the database"""
     with Session(engine) as session:
-        for _process in psutil.process_iter():
-            with _process.oneshot():
-                _sysprocess = models.build_sysprocess_from_process(_process)
+        while True:
+            data = data_queue.get()
+            if data is None:
+                session.commit()
+                return
 
-                _cpu = models.build_cpu_from_process(_process)
-                _memory = models.build_memory_from_process(_process)
-                _disk = models.build_disk_from_process(_process)
-
+            _sysprocess, _cpu, _memory, _disk = data
+            try:
                 session.merge(_sysprocess)
                 session.add_all([_cpu, _memory, _disk])
                 session.commit()
+            except OperationalError:
+                session.rollback()
+                logger.error(f"The database is locked. Retrying in {interval} seconds")
+
+            # Without a sleep here the chance of DB lock is much higher and
+            # query performance is noticably slower
+            sleep(interval)
